9a2e1189cce2c93e61ffc0099abfb31d233909de
[DudiyMaksim] <[maksdudiya25@gmail.com]>
2024-03-18 21:43:29 +0200

    Add project files.

---
+++ Topic Cycles.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	///*task 1.1*/
+	//int a, sum=0;
+	//cout << "Enter a : ";
+	//cin >> a;
+	//for (a; a!=501; a++)
+	//{
+	//	sum += a;
+	//}
+	//cout << sum;
+	///*task 1.2*/
+	//int x,y,i=1,a;
+	//cout << "Enter x : ";
+	//cin >> x;
+	//cout << "Enter y : ";
+	//cin >> y;
+	//a = x;
+	//for (i;  i!=y; i++)
+	//{
+	//	a =a* x;
+	//}
+	//cout << a;
+	///*task 1.3*/
+	//int i = 1,all=0;
+	//for (i ; i < 1001; i++)
+	//{
+	//	all += i;
+	//}
+	//cout << all/1000;
+	///*task 1.4*/
+	//short a, d=1;
+	//cout << "Enter a : ";
+	//cin >> a;
+	//for (a; a < 21; a++)
+	//{
+	//	d *= a;
+	//}
+	//cout << d;
+	///*task 1.5*/
+	//short k,i=1;
+	//cout << "Enter k : ";
+	//cin >> k;
+	//for (i; i<11; i++)
+	//{
+	//	cout << k << " x " << i << " = " << k * i << endl;
+	//}
+	///*task 2.1*/
+	//int i=100, ans = 0, first_n, secon_n, three_n;
+	//for (i; i<1000;i++)
+	//{
+	//	if ((i % 10)==((i / 10) % 10))
+	//	{
+	//		++ans;
+	//	}
+	//	else if (((i / 10) % 10)==(i / 100))
+	//	{
+	//		++ans;
+	//	}
+	//	else if ((i % 10) == (i / 100))
+	//	{
+	//		++ans;
+	//	}
+	//}
+	//cout << ans;
+	///*task 2.2*/
+	//int i=100, ans = 0, first_n, secon_n, three_n;
+	//for (i; i<1000;i++)
+	//{
+	//	if ((i % 10)!=((i / 10) % 10) && ((i / 10) % 10) != (i / 100) && (i % 10) != (i / 100))
+	//	{
+	//		++ans;
+	//	}
+	//}
+	//cout << ans;
+	///*task 2.4*/
+	//int A, B=1;
+	//cout << "Enter A :";
+	//cin >> A;
+	//for (B; B <A+1; B++)
+	//{
+	//	if ((A%(B*B)==0) && (A % (B * B * B) == 0))
+	//	{
+	//		cout << B << endl;
+	//	}
+	//}
+}
